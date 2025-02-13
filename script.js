07775c6bd85923177629f9fb97421a1b6122dc8f
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-02-13 22:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function isSameType(value1, value2) {
-	if (typeof(value1)===typeof(value2)) {
+	if (isNaN(value1) && isNaN(value2)) {
 		return true;
-	} else{
-		return false;
-	}
+	} 
+	return typeof(value1)===typeof(value2)
   //your js code here
 }
 
